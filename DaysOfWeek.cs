13d1a5660d9220c4e73408b0aee13175c247fa51
ahmedhh33 <ahmedhh3633@gmail.com>
2023-07-06 10:55:08 +0400

    DayOfWeek

---
+++ DaysOfWeek.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumTasks
+{
+    internal class DaysOfWeek
+    {
+        public DoW dow;
+
+        public DaysOfWeek(DoW dow)
+        {
+            this.dow=dow;
+        }
+
+        
+    }
+    enum DoW
+    {
+        SUNDAY,
+        MONDAY,
+        TUSDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY,
+    }
+}
