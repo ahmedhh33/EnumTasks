da3fcd6d132f667bc00029221fbe909ccf20b7b4
ahmedhh33 <ahmedhh3633@gmail.com>
2023-07-06 11:53:35 +0400

    task1

--- DaysOfWeek.cs
+++ DaysOfWeek.cs
@@ -15,6 +15,49 @@ namespace EnumTasks
             this.dow=dow;
         }
 
+        public static void Dayscheduale()
+        {
+            Console.WriteLine("Enter The Day");
+            string input = (Console.ReadLine());//input days
+            string days = input.ToUpper();//to upper letter
+            //Console.WriteLine(days);
+
+            DoW day;//giv enum name
+
+            if (Enum.TryParse(days, out day))//convert from enum to string it takes input string and out put nam of enum
+            {
+                switch (day)
+                {
+                    case DoW.SUNDAY:
+                        Console.WriteLine("You Do not have nothing scheduled");
+                        break;
+                    case DoW.MONDAY:
+                        Console.WriteLine("You Have To go to GYM");
+                        break;
+                    case DoW.TUSDAY:
+                        Console.WriteLine("You Have job test");
+                        break;
+                    case DoW.WEDNESDAY:
+                        Console.WriteLine("You Have job intervew");
+                        break;
+                    case DoW.THURSDAY:
+                        Console.WriteLine("you have nuthing");
+                        break;
+                    case DoW.FRIDAY:
+                        Console.WriteLine("Jomaa Prayer");
+                        break;
+                    case DoW.SATURDAY:
+                        Console.WriteLine("End the Break");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("You Entered wrong choice");
+            }
+        
+        }
+
         
     }
     enum DoW
